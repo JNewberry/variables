c44ed564dffd4200875985e6ed63bc6e7f7a4ac6
44518 <44518@longroad.ac.uk>
2014-09-09 15:05:58 +0100

    second commit

---
+++ first name.py
@@ -0,0 +1,8 @@
+#John Newberry
+#9/9/14
+#first_name
+first_name = input("please enter your name")
+#stores name as variable first_name
+print(first_name)
+#the name that was typed is printed
+print("Hi{0}!".format(first_name))
